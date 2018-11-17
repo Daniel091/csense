33af4526afadc2071dcdc8721c4fbbff4af29c92
m4nic <m4nic@unicorntoday.com>
2018-11-17 14:59:33 +0100

    added color changing for slider

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -51,7 +51,10 @@ function get_color() {
 
         var tmp = document.getElementById('current_image');
         if (tmp !== undefined) {
-            console.log(getAverageRGB(tmp));
+            color = getAverageRGB(tmp)
+            color_hex = rgbToHex(color.r, color.g, color.b)
+            elem = document.querySelector('.slider')
+            elem.pseudoStyle('before', 'background-color', color_hex)
         } else {
             console.log("No Image Available");
         }
@@ -59,6 +62,37 @@ function get_color() {
     });
 }
 
+var UID = {
+	_current: 0,
+	getNew: function(){
+		this._current++;
+		return this._current;
+	}
+};
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+HTMLElement.prototype.pseudoStyle = function(element,prop,value){
+	var _this = this;
+	var _sheetId = "pseudoStyles";
+	var _head = document.head || document.getElementsByTagName('head')[0];
+	var _sheet = document.getElementById(_sheetId) || document.createElement('style');
+	_sheet.id = _sheetId;
+	var className = "pseudoStyle" + UID.getNew();
+	
+	_this.className +=  " "+className; 
+	
+	_sheet.innerHTML += " ."+className+":"+element+"{"+prop+":"+value+"}";
+	_head.appendChild(_sheet);
+	return this;
+};
 
 function getAverageRGB(imgEl) {
 
