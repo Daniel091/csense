232660fe0f5419723716a827c0c3ad4857b5ecb6
daniel_eichinger <d.eichinger@campus.lmu.de>
2018-11-17 11:46:10 +0100

    adds screenshot feature

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -9,11 +9,33 @@ chrome.storage.sync.get('color', function (data) {
 
 // sets green for everything
 changeColor.onclick = function (ele) {
+    console.log('Button Click');
     let color = ele.target.value;
     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        console.log(tabs);
         chrome.tabs.executeScript(
             tabs[0].id,
             {code: 'document.body.style.backgroundColor = "' + color + '";'});
     });
-
 };
+
+let auto_switch = document.getElementById('auto_switch');
+let checked = false;
+auto_switch.onclick = function (ele) {
+    console.log('Checkbox Click');
+    checked = !checked;
+
+    if (checked) {
+        console.log('Checkbox Checked');
+        get_color();
+    }
+};
+
+function get_color() {
+    chrome.tabs.captureVisibleTab(null, {quality: 50}, function (image) {
+        console.log("Capture visible tap");
+
+        let screen_con = document.getElementById('screen_con');
+        screen_con.innerHTML = "<img src='" + image + "'/>";
+    });
+}
